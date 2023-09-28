df2aea1d81c6be35558b9368614484f17779e6a0
b-regan11 <reganbill02@gmail.com>
2023-09-28 09:26:26 -0400

    The Broc is Cooking

--- src/NPCs/Dinosaur.java
+++ src/NPCs/Dinosaur.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 public class Dinosaur extends NPC {
 
     public Dinosaur(int id, Point location) {
-        super(id, location.x, location.y, new SpriteSheet(ImageLoader.load("Pineapple Enemy.png"), 14, 17), "STAND_LEFT");
+        super(id, location.x, location.y, new SpriteSheet(ImageLoader.load("the_broc.png"), 20, 20), "STAND_LEFT");
     }
 
     @Override
