7bc10086b7c4f2ebc59455d6e730d913171ad2b9
b-regan11 <reganbill02@gmail.com>
2023-10-07 18:50:16 -0400

    Enemy Class Update 10/9

--- src/Level/Enemies.java
+++ src/Level/Enemies.java
@@ -1,100 +1,127 @@
 package Level;
 
-import Engine.GraphicsHandler;
+import java.util.HashMap;
+
 import GameObject.Frame;
 import GameObject.SpriteSheet;
 import Utils.Direction;
 
-import java.util.HashMap;
-
-// This class is a base class for all enemies in the game -- all enemies should extend from it
-public class Enemies extends MapEntity{
+// This class is a base class for all Enemies in the game - all enemies should extend from it
+public class Enemies extends MapEntity
+{
     protected int id = 0;
 
-    public Enemies(int id, float x, float y, SpriteSheet spriteSheet, String startingAnimation) {
+    public Enemies(int id, float x, float y, SpriteSheet spriteSheet, String startingAnimation)
+    {
         super(x, y, spriteSheet, startingAnimation);
         this.id = id;
     }
 
-    public Enemies(int id, float x, float y, HashMap<String, Frame[]> animations, String startingAnimation) {
+    public Enemies(int id, float x, float y, HashMap<String, Frame[]> animations, String startingAnimation)
+    {
         super(x, y, animations, startingAnimation);
         this.id = id;
     }
 
-    public Enemies(int id, float x, float y, Frame[] frames) {
+    public Enemies(int id, float x, float y, Frame[] frames)
+    {
         super(x, y, frames);
         this.id = id;
     }
 
-    public Enemies(int id, float x, float y, Frame frame) {
+    public Enemies(int id, float x, float y, Frame frame)
+    {
         super(x, y, frame);
         this.id = id;
     }
 
-    public Enemies(int id, float x, float y) {
+    public Enemies(int id, float x, float y)
+    {
         super(x, y);
         this.id = id;
     }
 
-    public int getId() { return id; }
+    public int getID()
+    {
+        return id;
+    }
 
-    public void facePlayer(Player player) {
-        if (Math.round(getBoundsX2()) - (getBounds().getWidth() / 2) < Math.round(player.getBoundsX2())) {
+    public void facePlayer(Player player)
+    {
+        if (Math.round(getBoundsX2()) - (getBounds().getWidth() / 2) < Math.round(player.getBoundsX2()))
+        {
             this.currentAnimationName = "STAND_RIGHT";
         }
-        else if (Math.round(getBoundsX1()) + (getBounds().getWidth() / 2) > Math.round(player.getBoundsX1())) {
+        else if (Math.round(getBoundsX1()) + (getBounds().getWidth() / 2) > Math.round(player.getBoundsX1()))
+        {
             this.currentAnimationName = "STAND_LEFT";
         }
     }
 
-    public void stand(Direction direction) {
-        if (direction == Direction.RIGHT) {
-            this.currentAnimationName = "STAND_RIGHT";
+    public void stand(Direction direction) 
+    {
+        if (direction == Direction.RIGHT)
+        {
+            this.currentAnimationName = "STAND_Right";
         }
-        else if (direction == Direction.LEFT) {
+        else if (direction == Direction.LEFT)
+        {
             this.currentAnimationName = "STAND_LEFT";
         }
     }
 
-    public void walk(Direction direction, float speed) {
-        if (direction == Direction.RIGHT) {
+    public void walk(Direction direction, float speed)
+    {
+        if (direction == Direction.RIGHT)
+        {
             this.currentAnimationName = "WALK_RIGHT";
         }
-        else if (direction == Direction.LEFT) {
+        else if (direction == Direction.LEFT)
+        {
             this.currentAnimationName = "WALK_LEFT";
         }
-        else {
-            if (this.currentAnimationName.contains("RIGHT")) {
+        else
+        {
+            if (this.currentAnimationName.contains("RIGHT")) 
+            {
                 this.currentAnimationName = "WALK_RIGHT";
             }
-            else {
+            else
+            {
                 this.currentAnimationName = "WALK_LEFT";
             }
         }
-        if (direction == Direction.UP) {
+        if (direction == Direction.UP)
+        {
             moveY(-speed);
         }
-        else if (direction == Direction.DOWN) {
+        else if (direction == Direction.DOWN)
+        {
             moveY(speed);
         }
-        else if (direction == Direction.LEFT) {
+        else if (direction == Direction.LEFT)
+        {
             moveX(-speed);
         }
-        else if (direction == Direction.RIGHT) {
+        else if (direction == Direction.RIGHT)
+        {
             moveX(speed);
         }
     }
 
-    public void update(Player player) {
+    public void update(Player player)
+    {
         super.update();
     }
 
     @Override
-    public void draw(GraphicsHandler graphicsHandler) {
+    public void draw(GraphicsHandler graphicsHandler)
+    {
         super.draw(graphicsHandler);
     }
 
-    public int getID(){
+    public int getID()
+    {
         return id;
     }
 }
