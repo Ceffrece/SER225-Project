aa38e8ea7748e2f55f8971e9ac0daea9f244760b
doubleupdingo <maferreira@quinnipiac.edu>
2023-10-24 18:02:46 -0400

    Made hub map, not playable yet

---
+++ src/Maps/HubMap.java
@@ -0,0 +1,26 @@
+package Maps;
+
+import Level.Map;
+import Tilesets.Floor1Tileset;
+import Scripts.SimpleTextScript;
+
+public class HubMap extends Map {
+
+    public HubMap() {
+        super("hub_map.txt", new Floor1Tileset());
+        this.playerStartPosition = getMapTile(12, 14).getLocation();
+
+    }
+
+    @Override
+    public void loadScripts() {
+        getMapTile(7, 12).setInteractScript(new SimpleTextScript("This is where you can choose your ultimate ability\n(once unlocked)")); 
+
+        getMapTile(17,12).setInteractScript(new SimpleTextScript("This is where you can choose your starting weapon\n(once unlocked)"));
+
+        getMapTile(11, 19).setInteractScript(new SimpleTextScript("This is an open area for you to test your weapons/abilities!")); 
+
+        getMapTile(11,6).setInteractScript(new SimpleTextScript("Enter this door to enter the Food Pyramid and\nbegin your run!"));
+    }
+    
+}
