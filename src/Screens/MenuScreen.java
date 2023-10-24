24aff6a49075ffc9b47392abc2f7aecef56285f7
krmacdonald <krmacdonald@quinnipiac.edu>
2023-10-24 17:52:26 -0400

    Added new title screen, added menu options, not functional yet :3

--- src/Screens/MenuScreen.java
+++ src/Screens/MenuScreen.java
@@ -3,6 +3,7 @@ package Screens;
 import Engine.*;
 import Game.GameState;
 import Game.ScreenCoordinator;
+import GameObject.Sprite;
 import Level.Map;
 import Maps.TitleScreenMap;
 import SpriteFont.SpriteFont;
@@ -17,11 +18,16 @@ public class MenuScreen extends Screen {
     protected SpriteFont playGame;
     protected SpriteFont credits;
     protected SpriteFont marcusMap; // new map for test purposes
+    protected SpriteFont instructions;
     protected Map background;
     protected int keyPressTimer;
     protected int pointerLocationX, pointerLocationY;
     protected KeyLocker keyLocker = new KeyLocker();
+    protected Sprite titleScreen = new Sprite(ImageLoader.load("TitleScreen.png"), 0, 0);
     public static boolean gameStarted;
+    GraphicsHandler graphicsHandler = new GraphicsHandler();
+    ScreenManager screenManager = new ScreenManager();
+    
 
     public MenuScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
@@ -29,15 +35,18 @@ public class MenuScreen extends Screen {
 
     @Override
     public void initialize() {
-        playGame = new SpriteFont("PLAY", 200, 119, "Comic Sans", 30, new Color(49, 207, 240));
+        playGame = new SpriteFont("PLAY", 560, 150, "Comic Sans", 20, new Color(49, 207, 240));
         playGame.setOutlineColor(Color.black);
         playGame.setOutlineThickness(3);
-        credits = new SpriteFont("CREDITS", 200, 219, "Comic Sans", 30, new Color(49, 207, 240));
+        credits = new SpriteFont("CREDITS", 545, 200, "Comic Sans", 20, new Color(49, 207, 240));
         credits.setOutlineColor(Color.black);
         credits.setOutlineThickness(3);
-        marcusMap = new SpriteFont("MARCUS", 200, 319, "Comic Sans", 30, new Color(49, 207, 240));
+        marcusMap = new SpriteFont("MARCUS", 545, 250, "Comic Sans", 20, new Color(49, 207, 240));
         marcusMap.setOutlineColor(Color.black);
         marcusMap.setOutlineThickness(3);
+        instructions = new SpriteFont("INSTRUCTIONS", 510, 300, "Comic Sans", 20, new Color(49, 207, 240));
+        instructions.setOutlineColor(Color.black);
+        instructions.setOutlineThickness(3);
         background = new TitleScreenMap();
         background.setAdjustCamera(false);
         keyPressTimer = 0;
@@ -48,12 +57,11 @@ public class MenuScreen extends Screen {
     public void update() {
         // update background map (to play tile animations)
         background.update(null);
-
         // if down or up is pressed, change menu item "hovered" over (blue square in front of text will move along with currentMenuItemHovered changing)
-        if (Keyboard.isKeyDown(Key.DOWN) && keyPressTimer == 0) {
+        if (Keyboard.isKeyDown(Key.S) && keyPressTimer == 0) {
             keyPressTimer = 14;
             currentMenuItemHovered++;
-        } else if (Keyboard.isKeyDown(Key.UP) && keyPressTimer == 0) {
+        } else if (Keyboard.isKeyDown(Key.W) && keyPressTimer == 0) {
             keyPressTimer = 14;
             currentMenuItemHovered--;
         } else {
@@ -63,10 +71,10 @@ public class MenuScreen extends Screen {
         }
 
         // if down is pressed on last menu item or up is pressed on first menu item, "loop" the selection back around to the beginning/end
-        if (currentMenuItemHovered > 2) {
+        if (currentMenuItemHovered > 3) {
             currentMenuItemHovered = 0;
         } else if (currentMenuItemHovered < 0) {
-            currentMenuItemHovered = 2;
+            currentMenuItemHovered = 3;
         }
 
         // sets location for blue square in front of text (pointerLocation) and also sets color of spritefont text based on which menu item is being hovered
@@ -74,20 +82,30 @@ public class MenuScreen extends Screen {
             playGame.setColor(new Color(255, 215, 0));
             credits.setColor(new Color(49, 207, 240));
             marcusMap.setColor(new Color(49, 207, 240));
-            pointerLocationX = 170;
-            pointerLocationY = 130;
+            instructions.setColor(new Color(49, 207, 240));
+            pointerLocationX = 530;
+            pointerLocationY = 154;
         } else if (currentMenuItemHovered == 1) {
             playGame.setColor(new Color(49, 207, 240));
             credits.setColor(new Color(255, 215, 0));
             marcusMap.setColor(new Color(49, 207, 240));
-            pointerLocationX = 170;
-            pointerLocationY = 230;
+            instructions.setColor(new Color(49, 207, 240));
+            pointerLocationX = 515;
+            pointerLocationY = 204;
         } else if (currentMenuItemHovered == 2) {
             playGame.setColor(new Color(49, 207, 240));
             credits.setColor(new Color(49, 207, 240));
             marcusMap.setColor(new Color(255, 215, 0));
-            pointerLocationX = 170;
-            pointerLocationY = 330;
+            instructions.setColor(new Color(49, 207, 240));
+            pointerLocationX = 515;
+            pointerLocationY = 254;
+        } else if(currentMenuItemHovered == 3){
+            playGame.setColor(new Color(49, 207, 240));
+            credits.setColor(new Color(49, 207, 240));
+            marcusMap.setColor(new Color(49, 207, 240));
+            instructions.setColor(new Color(255, 215, 0));
+            pointerLocationX = 480;
+            pointerLocationY = 304;
         }
 
         // if space is pressed on menu item, change to appropriate screen based on which menu item was chosen
@@ -104,15 +122,19 @@ public class MenuScreen extends Screen {
             } else if (menuItemSelected == 2) {
                 screenCoordinator.setGameState(GameState.MARCUS);
                 gameStarted = true;
+            } else if(menuItemSelected == 3){
+                screenCoordinator.setGameState(GameState.INSTRUCTIONS);
             }
         }
     }
 
     public void draw(GraphicsHandler graphicsHandler) {
         background.draw(graphicsHandler);
+        titleScreen.draw(graphicsHandler);
         playGame.draw(graphicsHandler);
         credits.draw(graphicsHandler);
         marcusMap.draw(graphicsHandler);
+        instructions.draw(graphicsHandler);
         graphicsHandler.drawFilledRectangleWithBorder(pointerLocationX, pointerLocationY, 20, 20, new Color(49, 207, 240), Color.black, 2);
     }
     
