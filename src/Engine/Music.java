f8968e566b6c1f08e862fbd12c41f4f909cffc08
Ceffrece <cheffrece@quinnipiac.edu>
2023-10-28 15:34:30 -0400

    game now SOMETIMES plays a sound when player is firing

--- src/Engine/Music.java
+++ src/Engine/Music.java
@@ -46,4 +46,57 @@ public class Music {
             }
         
     }
+    //adds a way to make it not loop Ex: for a blast
+    public Music(String filePath,int key)
+        {
+            try{
+                //create audioinputstream object
+                audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+
+                //create clip reference
+                clip = AudioSystem.getClip();
+
+                //open audioinputstream to the clip
+                clip.open(audioInputStream);
+                clip.start();
+                //restart();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+            catch(UnsupportedAudioFileException e){
+                e.printStackTrace();
+            }
+            catch(LineUnavailableException e){
+                e.printStackTrace();
+            }
+        
+    }
+    //restarts clip
+    public void restart() throws IOException, LineUnavailableException, 
+                                            UnsupportedAudioFileException  
+    { 
+        clip.stop(); 
+        clip.close(); 
+        resetAudioStream(); 
+        currentFrame = 0L; 
+        clip.setMicrosecondPosition(0); 
+        this.play(); 
+    } 
+    // Method to reset audio stream 
+    public void resetAudioStream() throws UnsupportedAudioFileException, IOException, 
+                                            LineUnavailableException  
+    { 
+        audioInputStream = AudioSystem.getAudioInputStream( 
+        new File(filePath).getAbsoluteFile()); 
+        clip.open(audioInputStream); 
+        clip.loop(Clip.LOOP_CONTINUOUSLY); 
+    } 
+    public void play()  
+    { 
+        //start the clip 
+        clip.start(); 
+          
+        status = "play"; 
+    } 
 }   
