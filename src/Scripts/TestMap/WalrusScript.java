37b35381565597a21e66616a56f60bbf232e9d77
Ceffrece <cheffrece@quinnipiac.edu>
2023-10-30 13:47:00 -0400

    added some text for the walrus npc to go along with the story

--- src/Scripts/TestMap/WalrusScript.java
+++ src/Scripts/TestMap/WalrusScript.java
@@ -14,12 +14,15 @@ public class WalrusScript extends Script<NPC> {
 
         // changes what walrus says when talking to him the first time (flag is not set) vs talking to him afterwards (flag is set)
         if (!isFlagSet("hasTalkedToWalrus")) {
-            addTextToTextboxQueue( "Hi Cat!");
-            addTextToTextboxQueue( "...oh, you lost your ball?");
-            addTextToTextboxQueue( "Hmmm...my walrus brain remembers seeing Dino with\nit last. Maybe you can check with him?");
+            addTextToTextboxQueue( "Welcome Traveler! So you are the one the council of chefs has sent here... \n You don't look like much... Alright then, in you go!");
+            addTextToTextboxQueue( "What do you mean, what are you doing here? Didn't they tell you anything? I guess I can spare a minute to tell you.");
+            addTextToTextboxQueue( "Long ago, there was an ancient power introduced to this world, one that would change us forever. \n The chalice of flavor fell from the sky, and the culinary world was created.");
+            addTextToTextboxQueue("For years, the chalice has been guarded by the structure in front of you, the legendary food pyramid.");
+            addTextToTextboxQueue("But now, its guardians inside has been corrupted by the power and have become monsters seeking to destroy the world and remove all flavor!");
+            addTextToTextboxQueue("It is now up to you. You must enter the food pyramid and recover the chalice from the destructive guardians!");
         }
         else {
-            addTextToTextboxQueue( "I sure love doing walrus things!");
+            addTextToTextboxQueue( "Time is ticking... GET IN THERE!");
         }
         entity.facePlayer(player);
     }
