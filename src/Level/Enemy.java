f5b069c9d71b95ffecb0cf1266bb65595b5a9e22
b-regan11 <reganbill02@gmail.com>
2023-11-08 15:48:48 -0500

    Enemy hurts player properly

--- src/Level/Enemy.java
+++ src/Level/Enemy.java
@@ -10,7 +10,7 @@ public class Enemy extends MapEntity
 {
     protected int id = 0;
     protected int health;
-
+    
     public Enemy(int id, float x, float y, SpriteSheet spriteSheet, String startingAnimation)
     {
         super(x, y, spriteSheet, startingAnimation);
@@ -123,10 +123,14 @@ public class Enemy extends MapEntity
     {   
         facePlayer(player);
         walkTowardPlayer(player);
-        // if this.overlaps player then hurtPlayer
-        if (player.overlaps(this))
+        if(Player.invincibilityTimer > 0){
+            Player.invincibilityTimer -= 1;
+        }
+        // if this.overlaps player then hurtPlayer, enemies cannot be damaged by projectiles from this method
+        if (player.overlaps(this) && Player.invincibilityTimer == 0)
         {
             Player.hurtPlayer(this);
+            Player.invincibilityTimer = 180;
         }
         super.update();
     }
