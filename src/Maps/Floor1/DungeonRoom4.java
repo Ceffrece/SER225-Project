0b165a30da518309b2f7cc3a0870b3d1a0b5ab7d
doubleupdingo <maferreira@quinnipiac.edu>
2023-11-09 16:18:43 -0500

    less naked

--- src/Maps/Floor1/DungeonRoom4.java
+++ src/Maps/Floor1/DungeonRoom4.java
@@ -29,7 +29,20 @@ public class DungeonRoom4 extends Map {
         public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
             ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
     
-
+        Spikes spikes1 = new Spikes(getMapTile(2, 41).getLocation());
+        Spikes spikes2 = new Spikes(getMapTile(3, 41).getLocation());
+        Spikes spikes3 = new Spikes(getMapTile(8, 37).getLocation());
+        Spikes spikes4 = new Spikes(getMapTile(9, 35).getLocation());
+        Spikes spikes5 = new Spikes(getMapTile(10, 37).getLocation());
+        Spikes spikes6 = new Spikes(getMapTile(15, 41).getLocation());
+        Spikes spikes7 = new Spikes(getMapTile(16, 41).getLocation());
+        enhancedMapTiles.add(spikes1);
+        enhancedMapTiles.add(spikes2);
+        enhancedMapTiles.add(spikes3);
+        enhancedMapTiles.add(spikes4);
+        enhancedMapTiles.add(spikes5);
+        enhancedMapTiles.add(spikes6);
+        enhancedMapTiles.add(spikes7);
     
             return enhancedMapTiles;
         }
@@ -47,7 +60,11 @@ public class DungeonRoom4 extends Map {
     @Override
     public void loadScripts() {
 
-        getMapTile(5,36).setInteractScript(new RandomChangeMapScript(1));
+        getMapTile(17,31).setInteractScript(new SimpleTextScript("A naked woman? Nonsense!"));
+
+        getMapTile(1,31).setInteractScript(new SimpleTextScript("A kirby reference? That's absurd!"));
+
+        getMapTile(9,41).setInteractScript(new RandomChangeMapScript(1));
     }
     
 }
