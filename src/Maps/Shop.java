77f88ce3a7574dd7dead9d7c73dd8384f3c90087
Riley Damasco <rileydamasco@Rileys-MBP-4.quinnipiac.edu>
2023-11-14 18:16:33 -0500

    shop door goes to boss

--- src/Maps/Shop.java
+++ src/Maps/Shop.java
@@ -119,6 +119,9 @@ public class Shop extends Map {
     public void loadScripts() {
 
         getMapTile(12,2).setInteractScript(new RandomChangeMapScript(1));
+
+        getMapTile(12,6).setInteractScript(new ChangeMapScript(100));
+
     }
     
 }
