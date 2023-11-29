d1d08ce0f852f9fa1b64ecb2d1bfc7127fa73c28
doubleupdingo <maferreira@quinnipiac.edu>
2023-11-28 23:46:31 -0500

    turned off platino's hitbox

--- src/NPCs/Platino.java
+++ src/NPCs/Platino.java
@@ -65,9 +65,9 @@ public class Platino extends NPC {
 
             // Uncomment this to draw player's bounds to screen -- useful for debugging
             
-            if (this instanceof NPC) {
-                    drawBounds(graphicsHandler, new Color(255, 0, 0, 100));
-            }
+            //if (this instanceof NPC) {
+            //        drawBounds(graphicsHandler, new Color(255, 0, 0, 100));
+            //}
             
     } else {
             super.draw(graphicsHandler);
