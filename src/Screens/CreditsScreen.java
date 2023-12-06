e798c8dc3a64b92dfbaf76b62f212a635cd31c34
Ceffrece <cheffrece@quinnipiac.edu>
2023-12-06 16:29:35 -0500

    credits screen updated

--- src/Screens/CreditsScreen.java
+++ src/Screens/CreditsScreen.java
@@ -18,6 +18,13 @@ public class CreditsScreen extends Screen {
     protected SpriteFont createdByLabel;
     protected SpriteFont tilesetLabel;
     protected SpriteFont returnInstructionsLabel;
+    protected SpriteFont teamMembers;
+    protected SpriteFont caden;
+    protected SpriteFont marcus;
+    protected SpriteFont bill;
+    protected SpriteFont kyle;
+    protected SpriteFont riley;
+
 
     public CreditsScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
@@ -30,7 +37,14 @@ public class CreditsScreen extends Screen {
         background.setAdjustCamera(false);
         creditsLabel = new SpriteFont("Credits", 15, 7, "Times New Roman", 30, Color.white);
         createdByLabel = new SpriteFont("Created by Alex Thimineur and Team Let Us Cook", 15, 120, "Times New Roman", 25, Color.white);
-        tilesetLabel = new SpriteFont("Custom tileset Dawnlike by DragonDePlatino and DawnBringer", 15, 200, "Times New Roman", 25, Color.white);
+        teamMembers = new SpriteFont("Let Us Cook Team Members:", 15, 160, "Times New Roman", 25, Color.white);
+        caden = new SpriteFont("Caden Effrece", 15, 180, "Times New Roman", 25, Color.white);
+        marcus = new SpriteFont("Marcus Ferreria", 15, 200, "Times New Roman", 25, Color.white);
+        bill = new SpriteFont("Bill Regan", 15, 220, "Times New Roman", 25, Color.white);
+        kyle = new SpriteFont("Kyle MacDonald", 15, 240, "Times New Roman", 25, Color.white);
+        riley = new SpriteFont("Riley Damasco", 15, 260, "Times New Roman", 25, Color.white);
+
+        tilesetLabel = new SpriteFont("Custom tileset Dawnlike by DragonDePlatino and DawnBringer", 15, 500, "Times New Roman", 25, Color.white);
         returnInstructionsLabel = new SpriteFont("Press Space to return to the menu", 20, 532, "Times New Roman", 30, Color.white);
         keyLocker.lockKey(Key.SPACE);
     }
@@ -54,5 +68,11 @@ public class CreditsScreen extends Screen {
         createdByLabel.draw(graphicsHandler);
         tilesetLabel.draw(graphicsHandler);
         returnInstructionsLabel.draw(graphicsHandler);
+        teamMembers.draw(graphicsHandler);
+        caden.draw(graphicsHandler);
+        marcus.draw(graphicsHandler);
+        riley.draw(graphicsHandler);
+        bill.draw(graphicsHandler);
+        kyle.draw(graphicsHandler);
     }
 }
